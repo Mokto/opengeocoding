2f59498352fb2bf28697aedda308143d22e72d0e
Theo Mathieu <tmathieu.github@fastmail.com>
2023-09-09 16:03:26 +0200

    fix: missing rmq file

---
+++ api/pkg/apis/rabbitmq.go
@@ -0,0 +1,72 @@
+package apis
+
+import (
+	"database/sql"
+	"fmt"
+	"geocoding/pkg/graceful"
+	"log"
+
+	"github.com/wagslane/go-rabbitmq"
+)
+
+func StartRmqConsumer(gracefulManager *graceful.Manager, database *sql.DB, rmqConnection *rabbitmq.Conn) {
+
+	consumer, err := rabbitmq.NewConsumer(
+		rmqConnection,
+		func(d rabbitmq.Delivery) rabbitmq.Action {
+			_, err := database.Exec(string(d.Body))
+			fmt.Println(string(d.Body))
+			if err != nil {
+				fmt.Println(err)
+				if !d.Redelivered {
+					return rabbitmq.NackRequeue
+				} else {
+					return rabbitmq.NackDiscard
+				}
+			}
+			return rabbitmq.Ack
+		},
+		"main:::backgroundSave",
+		rabbitmq.WithConsumerOptionsConcurrency(1),
+		rabbitmq.WithConsumerOptionsQueueQuorum,
+		rabbitmq.WithConsumerOptionsQueueDurable,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gracefulManager.OnShutdown(func() {
+		consumer.Close()
+	})
+}
+
+type RmqPublisher struct {
+	publisher *rabbitmq.Publisher
+}
+
+func (publisher *RmqPublisher) Publish(queueName string, message string) error {
+	err := publisher.publisher.Publish([]byte(message), []string{queueName}) // rabbitmq.WithPublishOptionsExchange("events"),
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func StartRmqPublisher(gracefulManager *graceful.Manager, rmqConnection *rabbitmq.Conn) *RmqPublisher {
+
+	publisher, err := rabbitmq.NewPublisher(
+		rmqConnection,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gracefulManager.OnShutdown(func() {
+		defer publisher.Close()
+	})
+
+	return &RmqPublisher{
+		publisher: publisher,
+	}
+}
