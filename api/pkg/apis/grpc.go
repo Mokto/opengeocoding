fd60e99bfba22c6b5cc1221a1902029c8296bd3f
Theo Mathieu <tmathieu.github@fastmail.com>
2023-09-11 11:24:22 +0200

    fix: grpc listen on more than localhost

--- api/pkg/apis/grpc.go
+++ api/pkg/apis/grpc.go
@@ -57,7 +57,7 @@ func (s *opengeocodingServerInternal) RunBackgroundQuery(ctx context.Context, re
 func StartGrpc(gracefulManager *graceful.Manager, database *sql.DB, publisher *RmqPublisher) {
 
 	port := 8091
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
