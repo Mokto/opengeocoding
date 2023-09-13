95a915d69691fe751f4000078cfbc60032de00a0
Theo Mathieu <tmathieu.github@fastmail.com>
2023-09-13 16:10:27 +0200

    perfs: improved zone detector performances

--- importers/src/wof/zone_detector.rs
+++ importers/src/wof/zone_detector.rs
@@ -12,6 +12,7 @@ use std::time::Instant;
 use crate::extractor::tar_bz2::download_and_extract;
 
 struct ZoneData {
+    id: String,
     name: String,
     geometry: Geometry,
     centroid: Point,
@@ -19,54 +20,56 @@ struct ZoneData {
 
 pub struct ZoneDetector {
     zones: HashMap<String, ZoneData>,
-    zones_names: CollectingHashMap<String, String>,
+    zones_ids: CollectingHashMap<String, String>,
 }
 
 struct DataFile {
     country_code: String,
-    zone: String,
+    name: String,
     file_path: String,
+    id: String,
+    centroid: Option<Point>,
 }
 
 impl ZoneDetector {
     pub fn detect(&self, country_code: String, lat: f64, long: f64) -> Option<String> {
         let country_code = country_code.to_lowercase();
 
-        let zones_names = self.zones_names.get_all(&country_code);
-        if zones_names.is_none() {
+        let zones_ids = self.zones_ids.get_all(&country_code);
+        if zones_ids.is_none() {
             return None;
         }
 
-        let zones_names = zones_names.unwrap();
+        let zones_ids = zones_ids.unwrap();
 
         let point = point! {x: long, y: lat};
 
         // sort zones by distance from point to centroid. Will probably find the matching region first.
-        let mut zones_names = zones_names.to_vec();
-        zones_names.sort_by_cached_key(|zone_name| {
-            let centroid = &self
-                .zones
-                .get((country_code.clone() + zone_name.as_str()).as_str())
-                .unwrap()
-                .centroid;
+        let mut zones_ids = zones_ids.to_vec();
+        zones_ids.sort_by_cached_key(|zone_id| {
+            let centroid = &self.zones.get(zone_id).unwrap().centroid;
             (centroid.vincenty_distance(&point)).unwrap_or(f64::MAX) as u32
         });
 
-        for zone_name in zones_names {
-            if self
-                .zones
-                .get((country_code.clone() + zone_name.as_str()).as_str())
-                .unwrap()
-                .geometry
-                .contains(&point)
-            {
-                return Some(zone_name);
+        for zone_id in &zones_ids {
+            let zone = self.zones.get(zone_id).unwrap();
+            if zone.geometry.contains(&point) {
+                return Some(zone.name.clone());
             }
         }
 
         None
     }
-    pub async fn new(zone_type: &str) -> Self {
+
+    pub async fn new_region_detector() -> Self {
+        Self::new("region", 14).await
+    }
+    pub async fn new_locality_detector() -> Self {
+        unimplemented!("Locality detector not implemented yet. It's supposed to work but apparently always returns None.");
+        // Self::new("locality", 8).await
+    }
+
+    async fn new(zone_type: &str, lat_index: usize) -> Self {
         let zones_folder = format!("./data/whosonfirst-data-{}-latest", zone_type);
 
         download_and_extract(
@@ -81,7 +84,7 @@ impl ZoneDetector {
         .await
         .unwrap();
 
-        println!("Loading zones...");
+        println!("Loading {}s...", zone_type);
         let now = Instant::now();
 
         let file = fs::File::open(format!(
@@ -95,15 +98,29 @@ impl ZoneDetector {
 
         for result in rdr.records() {
             let record = result.unwrap();
+            let id = record.get(9).unwrap();
             let name = record.get(17).unwrap();
             let path = record.get(19).unwrap();
             let country_code = record.get(25).unwrap().to_lowercase();
+            let longitude = record.get(lat_index + 1).unwrap().parse::<f64>().unwrap();
+            let latitude = record.get(lat_index).unwrap().parse::<f64>().unwrap();
+            let centroid = if longitude != 0.0 || latitude != 0.0 {
+                Some(point! {x: longitude, y: latitude})
+            } else {
+                None
+            };
+            // println!(
+            //     "{}, {}, {}, {}, {}, {}",
+            //     id, name, path, country_code, longitude, latitude,
+            // );
 
             let path = zones_folder.clone() + "/data/" + path;
             records.push(DataFile {
                 country_code: country_code.clone(),
-                zone: name.to_string(),
+                name: name.to_string(),
+                id: id.to_string(),
                 file_path: path.clone(),
+                centroid: centroid,
             });
         }
         println!("Found {} zones to load.", records.len());
@@ -113,17 +130,29 @@ impl ZoneDetector {
             .map(|record| {
                 let file_data = fs::read_to_string(&record.file_path).unwrap();
 
-                let geojson = GeoJson(file_data.as_str());
-                if let Ok(Geometry::Point(_poly)) = geojson.to_geo() {
+                let geojson = GeoJson(file_data.as_str()).to_geo();
+                if let Ok(Geometry::Point(_poly)) = geojson {
                     return None;
                 }
-                match geojson.to_geo() {
+                match geojson {
                     Ok(data) => {
+                        if record.centroid.is_some() {
+                            return Some((
+                                record.country_code.clone(),
+                                ZoneData {
+                                    geometry: data,
+                                    name: record.name.clone(),
+                                    id: record.id.clone(),
+                                    centroid: record.centroid.unwrap(),
+                                },
+                            ));
+                        }
                         return Some((
                             record.country_code.clone(),
                             ZoneData {
                                 geometry: data.clone(),
-                                name: record.zone.clone(),
+                                name: record.name.clone(),
+                                id: record.id.clone(),
                                 centroid: data.centroid().unwrap(),
                             },
                         ));
@@ -131,29 +160,29 @@ impl ZoneDetector {
                     Err(_) => return None,
                 }
             })
-            .into_par_iter()
-            .flatten();
+            .flatten()
+            .collect::<Vec<_>>();
 
-        let hashmap_result = zones_iter
-            .clone()
-            .map(|region| (region.0.to_string() + region.1.name.as_str(), region.1))
-            .collect::<Vec<_>>()
-            .into_iter()
-            .collect::<HashMap<_, _>>();
-
-        let zones_names = zones_iter
-            // .map(|d| => (d.0.clone(), d.1.name.clone()))
-            .map(|region| (region.0.clone(), region.1.name.clone()))
+        let zones_ids = zones_iter
+            .iter()
+            .map(|(country, zone_data)| (country.clone(), zone_data.id.clone()))
             .collect::<Vec<_>>()
             .into_iter()
             .collect::<CollectingHashMap<_, _>>();
 
+        let hashmap_result = zones_iter
+            .into_par_iter()
+            .map(|(_, zone_data)| (zone_data.id.clone(), zone_data))
+            .collect::<Vec<_>>()
+            .into_iter()
+            .collect::<HashMap<_, _>>();
+
         let elapsed = now.elapsed();
-        println!("Took {:.2?}\n", elapsed);
+        println!("Took {:.2?}", elapsed);
 
         Self {
             zones: hashmap_result,
-            zones_names: zones_names,
+            zones_ids: zones_ids,
         }
     }
 }
